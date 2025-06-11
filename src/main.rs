325ce0e9460fdcccc11cb056e576fbe3a919082e
Jannik Novak <nevisha@pm.me>
2025-06-11 06:42:43 +0200

    main.rs aktualisieren

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,6 @@ mod handlers;
 
 use handlers::*;
 
-// global variables for the handlers that will be initialized in main
 static mut CHAT_HANDLER: ChatHandler = ChatHandler { context: None };
 static mut SUMMARIZE_HANDLER: SummarizeHandler = SummarizeHandler {};
 
@@ -62,36 +61,26 @@ impl EventHandler for Handler {}
 
 #[tokio::main]
 async fn main() {
-    // Initialize the handlers
     unsafe {
         CHAT_HANDLER = ChatHandler::new();
         SUMMARIZE_HANDLER = SummarizeHandler::new();
     }
 
-    // Load the .env file
     dotenv().ok();
-
-    // Get the discord bot token from the environment
     let token = env::var("DISCORD_TOKEN").expect("token");
 
-    // Create a new instance of the framework with a reference to the General group
-    // This GENERAL_GROUP gets created above by the #[group] macro for the struct named General
-    // Naming the group, for instance, Cool, would create an instance named COOL_GROUP
     let framework = StandardFramework::new()
         .configure(|c| c.prefix("!"))
         .group(&GENERAL_GROUP);
 
-    // Create intents for the bot
     let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
 
-    // Create a new instance of the client using the token, intents and framework
     let mut client = Client::builder(token, intents)
         .event_handler(Handler)
         .framework(framework)
         .await
         .expect("Error creating client");
 
-    // Start the client and print an error if it occurs during startup
     if let Err(why) = client.start().await {
         println!("An error occurred while running the client: {:?}", why);
     }
