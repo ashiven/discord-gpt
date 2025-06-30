44ada9e2f989811e38e91315cec8b0df98bf3853
ashiven <nevisha@pm.me>
2025-06-30 12:19:28 +0200

    switch to prefix commands and move commands into own module

---
+++ src/commands.rs
@@ -0,0 +1,51 @@
+use crate::handlers::CommandHandler;
+use crate::{Context, Data, Error};
+use once_cell::sync::Lazy;
+use tokio::sync::Mutex;
+
+static COMMAND_HANDLER: Lazy<Mutex<CommandHandler>> =
+    Lazy::new(|| Mutex::new(CommandHandler::new()));
+
+#[poise::command(prefix_command, on_error = "error_handler")]
+pub async fn chat(
+    ctx: Context<'_>,
+    #[description = "The message sent by the user"]
+    #[rest]
+    message: String,
+) -> Result<(), Error> {
+    let message = &message;
+    let author_id = ctx.author().id;
+    let command = &ctx.command().name;
+    let response = COMMAND_HANDLER
+        .lock()
+        .await
+        .handle(command, message, author_id)
+        .await?;
+    ctx.reply(response).await?;
+
+    Ok(())
+}
+
+#[poise::command(prefix_command, on_error = "error_handler")]
+pub async fn summarize(
+    ctx: Context<'_>,
+    #[description = "The message sent by the user"]
+    #[rest]
+    message: String,
+) -> Result<(), Error> {
+    let message = &message;
+    let author_id = ctx.author().id;
+    let command = &ctx.command().name;
+    let response = COMMAND_HANDLER
+        .lock()
+        .await
+        .handle(command, message, author_id)
+        .await?;
+    ctx.reply(response).await?;
+
+    Ok(())
+}
+
+async fn error_handler(error: poise::FrameworkError<'_, Data, Error>) {
+    println!("An error occurred: {error}");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,31 @@
 use dotenv::dotenv;
-use once_cell::sync::Lazy;
 use poise::serenity_prelude as serenity;
-use tokio::sync::Mutex;
 
 mod chat;
+mod commands;
 mod handlers;
-
-use handlers::*;
 
 struct Data {}
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
-
-static COMMAND_HANDLER: Lazy<Mutex<CommandHandler>> =
-    Lazy::new(|| Mutex::new(CommandHandler::new()));
-
-#[poise::command(slash_command, prefix_command)]
-async fn chat(ctx: Context<'_>, message: String) -> Result<(), Error> {
-    let message = &message;
-    let author_id = ctx.author().id;
-    let command = &ctx.command().name;
-    let response = COMMAND_HANDLER
-        .lock()
-        .await
-        .handle(command, message, author_id)
-        .await?;
-    ctx.reply(response).await?;
-
-    Ok(())
-}
-
-#[poise::command(slash_command, prefix_command)]
-async fn summarize(ctx: Context<'_>, message: String) -> Result<(), Error> {
-    let message = &message;
-    let author_id = ctx.author().id;
-    let command = &ctx.command().name;
-    let response = COMMAND_HANDLER
-        .lock()
-        .await
-        .handle(command, message, author_id)
-        .await?;
-    ctx.reply(response).await?;
-
-    Ok(())
-}
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
     let discord_token = std::env::var("DISCORD_TOKEN").expect("missing discord token");
 
-    let intents = serenity::GatewayIntents::non_privileged();
+    let intents =
+        serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT;
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![chat(), summarize()],
+            prefix_options: poise::PrefixFrameworkOptions {
+                prefix: Some("~".into()),
+                ..Default::default()
+            },
+            commands: vec![commands::chat(), commands::summarize()],
             ..Default::default()
         })
-        .setup(|ctx, _ready, framework| {
-            Box::pin(async move {
-                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-                Ok(Data {})
-            })
-        })
+        .setup(|_ctx, _ready, _framework| Box::pin(async move { Ok(Data {}) }))
         .build();
     let client = serenity::ClientBuilder::new(discord_token, intents)
         .framework(framework)
